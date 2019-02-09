dc86df53224cfeda481f8375f0bc7516f684c56b
Joe Nap <15986486+joenap@users.noreply.github.com>
2019-02-08 20:40:19 -0500

    add back commented post

--- test/test_httpstream.py
+++ test/test_httpstream.py
@@ -15,6 +15,14 @@ async def test_should_return_response_on_200(mock_get, event_loop):
 
     mock_get.assert_called_once_with(URL)
 
+# @patch('aiohttp.ClientSession.post')
+# async def test_should_return_response_on_200(mock_post, event_loop):
+#     mock_post.return_value.__aenter__.return_value = CoroutineMock(status=200, spec=ClientResponse)
+#
+#     async with ClientSession(loop=event_loop) as session:
+#         await httpstream.post2(session, URL, DATA)
+#
+#     mock_post.assert_called_once_with(URL, data=DATA)
 
 # @patch('aiohttp.ClientSession.post')
 # async def test_should_print_error_when_not_200(mock_post, event_loop):
