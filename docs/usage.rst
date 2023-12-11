81087d8d1859b6be14d299ca8d0c5eb9ca284ff2
Joe Nap <15986486+joenap@users.noreply.github.com>
2023-12-11 12:02:52 -0500

    it installs

--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-========
-Usage
-========
-
-To use httpstream in a project::
-
-	import httpstream
--- httpstream/httpstream.py
+++ httpstream/httpstream.py
@@ -3,71 +3,71 @@ from collections import namedtuple
 from tornado.platform.asyncio import AnyThreadEventLoopPolicy
 
 import asyncio
-import aiohttp
-import itertools
-import threading
+# import aiohttp
+# import itertools
+# import threading
 
-# todo check out aiodns resolver
-# https://stackoverflow.com/a/45169094/1102470
-
-Response = namedtuple('Response', ['request', 'status', 'reason', 'text'])
-
-# Used to flush the response queue and stop the iterator.
-STOP_SENTINEL = {}
-
-
-def grouper(n, iterable):
-    """ Yields successive lists of size n from iterable """
-    it = iter(iterable)
-    while True:
-        chunk = tuple(itertools.islice(it, n))
-        if not chunk:
-            return
-        yield chunk
-
-
-async def send(client, request):
-    """ Handles a single request """
-    async with client.get(request) as response:
-        return Response(
-            request=request,
-            status=response.status,
-            reason=response.reason,
-            text=await response.text(),
-            json=await response.json(),
-        )
-
-
-async def send_chunk(client, requests):
-    """ Handles a chunk of requests asynchronously """
-    tasks = (asyncio.ensure_future(send(client, r)) for r in requests)
-    return await asyncio.gather(*tasks)
-
-
-async def send_stream(requests, sync_queue, concurrency_limit):
-    """ Handles a stream of requests and pushes responses to a queue """
-    async with aiohttp.ClientSession() as client:
-        # Gather responses in chunks of size concurrency_limit
-        for request_chunk in grouper(concurrency_limit, requests):
-            for response in await send_chunk(client, request_chunk):
-                sync_queue.put(response)
-        sync_queue.put(STOP_SENTINEL)
-
-
-def response_generator(sync_queue):
-    """ Wrap a standard queue with a generator """
-    while True:
-        response = sync_queue.get()
-        if response is STOP_SENTINEL:
-            return
-        yield response
-
-
-def worker(loop, pending_tasks):
-    loop.run_until_complete(asyncio.gather(*pending_tasks))
-    loop.close()
-
-
+# # todo check out aiodns resolver
+# # https://stackoverflow.com/a/45169094/1102470
+#
+# Response = namedtuple('Response', ['request', 'status', 'reason', 'text'])
+#
+# # Used to flush the response queue and stop the iterator.
+# STOP_SENTINEL = {}
+#
+#
+# def grouper(n, iterable):
+#     """ Yields successive lists of size n from iterable """
+#     it = iter(iterable)
+#     while True:
+#         chunk = tuple(itertools.islice(it, n))
+#         if not chunk:
+#             return
+#         yield chunk
+#
+#
+# async def send(client, request):
+#     """ Handles a single request """
+#     async with client.get(request) as response:
+#         return Response(
+#             request=request,
+#             status=response.status,
+#             reason=response.reason,
+#             text=await response.text(),
+#             json=await response.json(),
+#         )
+#
+#
+# async def send_chunk(client, requests):
+#     """ Handles a chunk of requests asynchronously """
+#     tasks = (asyncio.ensure_future(send(client, r)) for r in requests)
+#     return await asyncio.gather(*tasks)
+#
+#
+# async def send_stream(requests, sync_queue, concurrency_limit):
+#     """ Handles a stream of requests and pushes responses to a queue """
+#     async with aiohttp.ClientSession() as client:
+#         # Gather responses in chunks of size concurrency_limit
+#         for request_chunk in grouper(concurrency_limit, requests):
+#             for response in await send_chunk(client, request_chunk):
+#                 sync_queue.put(response)
+#         sync_queue.put(STOP_SENTINEL)
+#
+#
+# def response_generator(sync_queue):
+#     """ Wrap a standard queue with a generator """
+#     while True:
+#         response = sync_queue.get()
+#         if response is STOP_SENTINEL:
+#             return
+#         yield response
+#
+#
+# def worker(loop, pending_tasks):
+#     loop.run_until_complete(asyncio.gather(*pending_tasks))
+#     loop.close()
+#
+#
 def streamer(requests, concurrency_limit=1000):
     """
     Returns a generator of HTTP responses for the given generator of HTTP requests.
@@ -87,11 +87,12 @@ def streamer(requests, concurrency_limit=1000):
     sync_queue = Queue(concurrency_limit)
 
     asyncio.set_event_loop_policy(AnyThreadEventLoopPolicy())
-    loop = asyncio.get_event_loop()
-    loop.create_task(send_stream(requests, sync_queue, concurrency_limit))
-    pending_tasks = asyncio.all_tasks()
-    threading.Thread(name='worker', target=worker, args=(loop, pending_tasks)).start()
-    return response_generator(sync_queue)
+    # loop = asyncio.get_event_loop()
+    # loop.create_task(send_stream(requests, sync_queue, concurrency_limit))
+    # pending_tasks = asyncio.all_tasks()
+    # threading.Thread(name='worker', target=worker, args=(loop, pending_tasks)).start()
+    # return response_generator(sync_queue)
+    return [1,2,3]
 
 
 NUM_URLS = 1000
